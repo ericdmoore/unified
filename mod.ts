2e2657e73ad31f0dfc9ff8d68ffbe32e94c40f16
Eric Moore <eric.d.moore@gmail.com>
2022-02-28 14:41:33 -0600

    remove console log

--- mod.ts
+++ mod.ts
@@ -1,8 +1,1 @@
-import {unified as u} from './lib/index.js'
-// import type {unified as IUnified} from './lib/index.d.ts'
-
-export const unified = u // as IUnified
-export default unified
-
-
-console.log(unified)
+export {unified, unified as default} from './lib/index.js'
